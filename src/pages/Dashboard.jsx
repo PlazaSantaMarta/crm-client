833d7960a667d618abbee0423bf86a192a867f96
naobregon27 <na.obregon27@hotmail.com>
2025-06-11 19:52:25 -0300

    trabajando con ale 2.0

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -135,11 +135,11 @@ function Dashboard() {
   // Función para conectar con Google
   const handleConnectGoogle = async () => {
     try {
-      const response = await api.get('/api/google');
+      const response = await api.get('/auth/google');
       if (response.data?.authUrl) {
         // Abre la ventana de autenticación de Google
         const newWindow = window.open(response.data.authUrl, 'Google Auth', 'width=600,height=800,left=' + (window.screenX + (window.outerWidth - 600) / 2) + ',top=' + (window.screenY + (window.outerHeight - 800) / 2));
-        console.log(newWindow);
+        console.log("recibimos esto" + newWindow);
         setAuthWindow(newWindow);
         // Monitorear si la ventana se cierra
         const checkWindow = setInterval(() => {
